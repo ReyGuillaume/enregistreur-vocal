c216b7bc0b9a80c680fa0e5f1ef26ce9b696774f
Guillaume Rey <grey02@orange.fr>
2023-09-27 09:24:07 +0200

    feat : main activity

--- app/src/main/java/fr/guillaumerey/enregistreurvocal/activity/MainActivity.kt
+++ app/src/main/java/fr/guillaumerey/enregistreurvocal/activity/MainActivity.kt
@@ -2,11 +2,14 @@ package fr.guillaumerey.enregistreurvocal.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import fr.guillaumerey.enregistreurvocal.R
+import fr.guillaumerey.enregistreurvocal.adapter.AudioAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val list: RecyclerView = findViewById(R.id.audio_list)
+        list.adapter = AudioAdapter()
     }
 }
