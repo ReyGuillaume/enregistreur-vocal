9ef09aa1c9864975f084ece420694c687d9a10ef
nocxxy <evan.meziere@gmail.com>
2023-09-27 12:28:05 +0200

    debug realiser

--- app/src/main/java/fr/guillaumerey/enregistreurvocal/activity/MainActivity.kt
+++ app/src/main/java/fr/guillaumerey/enregistreurvocal/activity/MainActivity.kt
@@ -2,6 +2,10 @@ package fr.guillaumerey.enregistreurvocal.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.recyclerview.widget.RecyclerView
 import fr.guillaumerey.enregistreurvocal.R
 import fr.guillaumerey.enregistreurvocal.adapter.AudioAdapter
@@ -12,5 +16,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val list: RecyclerView = findViewById(R.id.audio_list)
         list.adapter = AudioAdapter()
+
+       val button = findViewById<View>(R.id.start_record)
+        button.setOnClickListener {
+            //val intent = Intent(applicationContext, RecordActivity::class.java)
+            //startActivity(intent)
+            Toast.makeText(applicationContext, "Coucou !", Toast.LENGTH_SHORT).show()
+        }
     }
 }
